96139ceca2916d784da25266d1b5b75c90beb7ad
Carter Munson <cartermm4@gmail.com>
2025-08-15 15:38:40 -0400

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,5 @@
-
+export class Store{
+  constructor(key){ this.key=key; }
+  async load(){ try{ return JSON.parse(localStorage.getItem(this.key))||[] }catch{ return [] } }
+  save(projects){ localStorage.setItem(this.key, JSON.stringify(projects)); }
+}
